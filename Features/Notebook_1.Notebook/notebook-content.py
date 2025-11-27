46b982c02935c31247428b46eadbab2d48430e29
Samruddhi <67478123+samruddhi-sies@users.noreply.github.com>
2025-11-27 15:55:55 +0530

    Committing 1 item from workspace ff136b77-1d11-4a10-bbcc-6221cc2bd160

--- Features/Notebook_1.Notebook/notebook-content.py
+++ Features/Notebook_1.Notebook/notebook-content.py
@@ -6,14 +6,25 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "db7c3ad9-c636-4c60-a270-25afcea2a6e4",
+# META       "default_lakehouse_name": "myLake1",
+# META       "default_lakehouse_workspace_id": "ff136b77-1d11-4a10-bbcc-6221cc2bd160",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "db7c3ad9-c636-4c60-a270-25afcea2a6e4"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
-
+print("hello")
 
 # METADATA ********************
 
