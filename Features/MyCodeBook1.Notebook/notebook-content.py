6f113b18a696a9f8221107f4c3fb4c81100dc27c
Samruddhi <67478123+samruddhi-sies@users.noreply.github.com>
2026-04-30 06:51:54 +0530

    Committing 1 item from workspace 0d21a510-8571-4382-8891-d6cdd634d4bc

--- Features/MyCodeBook1.Notebook/notebook-content.py
+++ Features/MyCodeBook1.Notebook/notebook-content.py
@@ -21,3 +21,14 @@
 # META   "language": "python",
 # META   "language_group": "synapse_pyspark"
 # META }
+
+# CELL ********************
+
+print("Hello")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
