bc833564586e0f11a698f26ecd507697181ef6dc
Samruddhi <67478123+samruddhi-sies@users.noreply.github.com>
2026-07-17 14:53:33 +0530

    Committing 1 item from workspace a481c6d7-12c3-4e46-a6ec-d70c4a2eebfb

--- Features/TranformationCodeBook1.Notebook/notebook-content.py
+++ Features/TranformationCodeBook1.Notebook/notebook-content.py
@@ -21,3 +21,14 @@
 # META   "language": "python",
 # META   "language_group": "synapse_pyspark"
 # META }
+
+# CELL ********************
+
+print("Welcome to DP700")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
